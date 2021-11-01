54e1fdf38504515c281a049830f3d9035c7f415c
andjhall <andjhall@umich.edu>
2021-10-31 20:23:54 -0400

    Figured out api call. Need to fix view and data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,15 +3,62 @@ import CoachCard from './CoachCard';
 import './Cards.css'
 
 
+class Cards extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      coaches: [],
+    };
+  }
+
+  componentDidMount(){
+    fetch(`http://localhost:5000/coaches/homepage-view`, {credentials: 'same-origin'})
+      .then((response) => {
+        if (!response.ok)
+          throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ coaches: data });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  render(){
+    return (
+      <div className="cards">
+        <h1>Meet our Coaches!</h1>
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              { !this.state.coaches ? <div>Loading...</div> : this.state.coaches.map((coach) => (
+                <CoachCard info={coach} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+/*
 function Cards() {
 
   // Get five coaches from api
   const [ fiveCoaches, setCoaches ] = React.useState(null);
 
   useEffect(() => {
-    fetch("localhost:5000/routes/coaches/homepage-view")
-      .then((res) => res.json())
-      .then((data) => setCoaches(data.message));
+    fetch(`http://localhost:5000/coaches/homepage-view`, { mode: 'no-cors', method: 'get' })
+      .then((response) => {
+        if (!response.ok)
+          throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => setCoaches(data.message))
+      .catch((error) => console.log(error));
+
   }, []);
 
   return (
@@ -20,7 +67,7 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            { fiveCoaches.map((coach) => (
+            { !fiveCoaches ? <div>Loading...</div> : fiveCoaches.map((coach) => (
               <CoachCard info={coach} />
             ))}
           </ul>
@@ -29,5 +76,5 @@ function Cards() {
     </div>
   )
 }
-
+*/
 export default Cards;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
 			<nav className="navbar">
 				<div className="navbar-container">
 					<Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-						CollegeCoaching <i class="navbar-logo-icon fas fa-graduation-cap"/>
+						CollegeCoaching <i className="navbar-logo-icon fas fa-graduation-cap"/>
 					</Link>
 					<div className="menu-icon" onClick={handleClick}>
 						<i className={click ? 'fas fa-times' : 'fas fa-bars'} />
