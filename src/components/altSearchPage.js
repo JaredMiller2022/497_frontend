5c38641d9fb760cb4dd35cf4c9231478960fef67
andjhall <andjhall@umich.edu>
2021-11-03 09:11:58 -0400

    working on temporary search page

--- src/components/altSearchPage.js
+++ src/components/altSearchPage.js
@@ -6,15 +6,54 @@ import CoachCard from './CoachCard'
 class AltSearchPage extends React.Component{
 
     constructor(props){
-
+        this.fetchUniversities = this.fetchUniversities.bind(this);
+        this.state = {
+            universities: [],
+            coaches: [],
+            isSchoolSelected: '',
+            searchQuery: ''
+        };
     }
 
     componentDidMount(){
 
     }
 
-    render(){
+    fetchData(){
+        fetch(`http://localhost:5000/coaches/[MAKE NEW API ROUTE]`, {credentials: 'same-origin'})
+        .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+        })
+        .then((data) => {
+            this.setState({ universities: data.universities });
+            
+            data.univerities.forEach(coach => {
+                fetch([coachURL])
+                .then((response) => {
+                    if (!response.ok) throw Error(response.statusText);
+                    return response.json();
+                })
+                .then((data) => {
+                    this.setState((prevState) => { coaches: prevState.coaches.concat(data) });
+                })
+                .catch((error) => console.log(error));
+            })
+        })
+        .catch((error) => console.log(error));
+    }
+
+    fetchUniversities(){
+        // This might need a new api route that returns
+        // a pair (university name, coach id)
+    }
+
+    fetchCoach(){
         
+    }
+
+    render(){
+
     }
 }
 
